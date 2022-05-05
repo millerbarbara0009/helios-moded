7078df80c0e6737fcffcc67a67dc78adf8427ba1
Snax-phycho <103353616+Snax-phycho@users.noreply.github.com>
2022-05-05 18:57:12 +0600

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -125,7 +125,7 @@ def get_readable_message():
                 globals()['PAGE_NO'] -= 1
             START = COUNT
         for index, download in enumerate(list(download_dict.values())[START:], start=1):
-            msg += f"\n<b>➜Name:</b> {download.name().replace('<', '')} "
+            msg += f"\n<b>➜Name:</b> <code>{download.name().replace('<', '')}</code>"
             msg += f"\n\n<b>➜Status:</b> <i>{download.status()}</i>"
             if download.status() not in [
                 MirrorStatus.STATUS_ARCHIVING,
