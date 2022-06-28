b8adbd7331f9634c2957f2d0311b998b56cd4e64
Snax-phycho <103353616+Snax-phycho@users.noreply.github.com>
2022-06-28 18:32:45 +0600

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -330,7 +330,7 @@ class MirrorListener:
                 msg += f'\n<b>➜SubFolders: </b>{folders}'
                 msg += f'\n<b>➜Files: </b>{files}'
             link = short_url(link)
-            buttons.buildbutton("DRIVE➜LINK", link)
+            buttons.buildbutton("DRIVE ➜ LINK", link)
             LOGGER.info(f'Done Uploading {name}')
             if INDEX_URL is not None:
                 url_path = requests.utils.quote(f'{name}')
@@ -338,10 +338,10 @@ class MirrorListener:
                 if ospath.isdir(f'{DOWNLOAD_DIR}/{self.uid}/{name}'):
                     share_url += '/'
                     share_url = short_url(share_url)
-                    buttons.buildbutton("INDEX➜LINK", share_url)
+                    buttons.buildbutton("INDEX ➜ LINK", share_url)
                 else:
                     share_url = short_url(share_url)
-                    buttons.buildbutton("INDEX➜LINK", share_url)
+                    buttons.buildbutton("INDEX ➜ LINK", share_url)
                     if VIEW_LINK:
                         share_urls = f'{INDEX_URL}/{url_path}?a=view'
                         share_urls = short_url(share_urls)
